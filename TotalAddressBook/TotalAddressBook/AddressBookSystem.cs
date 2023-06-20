d57b82f98069e8d87b0ec89e6b0714f6f7e7daef
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-20 16:35:51 +0530

    [Vaibhav] Add:Adding multiple book, each book has unique name

---
+++ TotalAddressBook/TotalAddressBook/AddressBookSystem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalAddressBook
+{
+    public class AddressBookSystem
+    {
+        private Dictionary<string, AddressBook> addressBooks;
+
+        public AddressBookSystem()
+        {
+            addressBooks = new Dictionary<string, AddressBook>();
+        }
+        public void AddAddressBook(string name)
+        {
+            if(!addressBooks.ContainsKey(name))
+            {
+                addressBooks[name] = new AddressBook();
+                Console.WriteLine($"Address book '{name}' created.");
+            }
+            else
+            {
+                Console.WriteLine($"Address book '{name}' already exists.");
+            }
+        }
+        public bool DisplayAddressBook(string name)
+        {
+            if(addressBooks.ContainsKey(name))
+            {
+                AddressBook book = addressBooks[name];
+                Console.WriteLine($"Address book: '{name}'");
+                book.DisplayContacts();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Address Book '{name}' does not exist.");
+                return false;
+            }
+        }
+        public AddressBook GetAddressBook(string name)
+        {
+            if (addressBooks.ContainsKey(name))
+            {
+                return addressBooks[name];
+            }
+
+            return null;
+        }
+    }
+}
--- TotalAddressBook/TotalAddressBook/Program.cs
+++ TotalAddressBook/TotalAddressBook/Program.cs
@@ -7,59 +7,87 @@ namespace TotalAddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book\n");
+            
+            AddressBookSystem addressBookSystem = new AddressBookSystem();
 
-            AddressBook book = new AddressBook();
-
-            Console.WriteLine("Add persons to the Address Book:");
+            Console.WriteLine("Address Book System");
 
             while (true)
             {
-                Console.WriteLine("Enter person details or 'q' to quit.");
+                Console.WriteLine("\nEnter an option:");
+                Console.WriteLine("1. Add a new Address Book");
+                Console.WriteLine("2. Display Address Book");
+                Console.WriteLine("3. Add Contact to Address Book");
+                Console.WriteLine("4. Quit");
 
-                Console.Write("First Name: ");
-                string firstName = Console.ReadLine();
+                string option = Console.ReadLine();
 
-                if (firstName.ToLower() == "q")
-                    break;
-
-                Console.Write("Last Name: ");
-                string lastName = Console.ReadLine();
-
-                Console.Write("Address: ");
-                string address = Console.ReadLine();
-
-                Console.Write("City: ");
-                string city = Console.ReadLine();
-
-                Console.Write("State: ");
-                string state = Console.ReadLine();
-
-                Console.Write("ZIP Code: ");
-                string zip = Console.ReadLine();
-
-                Console.Write("Phone Number: ");
-                string phoneNumber = Console.ReadLine();
-
-                Console.Write("Email: ");
-                string email = Console.ReadLine();
-
-                Contact contact = new Contact
+                switch (option)
                 {
-                    FirstName = firstName,
-                    LastName = lastName,
-                    Address = address,
-                    City = city,
-                    State = state,
-                    Zip = zip,
-                    PhoneNumber = phoneNumber,
-                    Email = email
-                };
+                    case "1":
+                        Console.Write("Enter the name for the Address Book: ");
+                        string addressBookName = Console.ReadLine();
+                        addressBookSystem.AddAddressBook(addressBookName);
+                        break;
+                    case "2":
+                        Console.Write("Enter the name of the Address Book to display: ");
+                        string displayAddressBookName = Console.ReadLine();
+                        addressBookSystem.DisplayAddressBook(displayAddressBookName);
+                        break;
+                    case "3":
+                        Console.Write("Enter the name of the Address Book to add a contact: ");
+                        string addContactAddressBookName = Console.ReadLine();
+                        if(addressBookSystem.DisplayAddressBook(addContactAddressBookName))
+                        {
+                            AddressBook book = addressBookSystem.GetAddressBook(addContactAddressBookName);
 
-                book.AddContact(contact);
+                            Console.Write("First Name: ");
+                            string firstName = Console.ReadLine();
+
+                            Console.Write("Last Name: ");
+                            string lastName = Console.ReadLine();
+
+                            Console.Write("Address: ");
+                            string address = Console.ReadLine();
+
+                            Console.Write("City: ");
+                            string city = Console.ReadLine();
+
+                            Console.Write("State: ");
+                            string state = Console.ReadLine();
+
+                            Console.Write("ZIP Code: ");
+                            string zip = Console.ReadLine();
+
+                            Console.Write("Phone Number: ");
+                            string phoneNumber = Console.ReadLine();
+
+                            Console.Write("Email: ");
+                            string email = Console.ReadLine();
+
+                            Contact contact = new Contact
+                            {
+                                FirstName = firstName,
+                                LastName = lastName,
+                                Address = address,
+                                City = city,
+                                State = state,
+                                Zip = zip,
+                                PhoneNumber = phoneNumber,
+                                Email = email
+                            };
+                            book.AddContact(contact);
+                            Console.WriteLine("Contact Added to the Address Book.");
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("Quitting the program...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
             }
-
-            Console.WriteLine("\nAll Contacts:");
-            book.DisplayContacts();
         }
     }
 }
