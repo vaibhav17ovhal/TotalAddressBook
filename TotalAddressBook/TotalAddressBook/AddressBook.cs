26c50ab2b7c550c513db63d6d289d58539fddf0b
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-20 13:00:32 +0530

    [Vaibhav] Add:Creating Contact in Address Book

---
+++ TotalAddressBook/TotalAddressBook/AddressBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalAddressBook
+{
+    public class AddressBook
+    {
+        private List<Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void DisplayContacts()
+        {
+            foreach(var contact in contacts)
+            {
+                Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+                Console.WriteLine($"Address: {contact.Address}, {contact.City}, {contact.State}, {contact.Zip}");
+                Console.WriteLine($"Phone: {contact.PhoneNumber}");
+                Console.WriteLine($"Email:{contact.Email}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TotalAddressBook/TotalAddressBook/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace TotalAddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book\n");
+
+            AddressBook book = new AddressBook();
+
+            Contact con1 = new Contact
+            {
+                FirstName = "Vaibhav",
+                LastName = "Ovhal",
+                Address = "Harsiddhi",
+                City = "Indore",
+                State = "M.P.",
+                Zip = "452007",
+                PhoneNumber = "9340596362",
+                Email = "vaibhavovhal15@gmail.com"
+            };
+
+            Contact con2 = new Contact
+            {
+                FirstName = "Ayushi",
+                LastName = "Dubey",
+                Address = "Juni Indore",
+                City = "Indore",
+                State = "M.P.",
+                Zip = "452007",
+                PhoneNumber = "7415887847",
+                Email = "ayushidubey80@gmail.com"
+            };
+            
+            Contact con3 = new Contact
+            {
+                FirstName = "Karan",
+                LastName = "Soni",
+                Address = "Annapurna",
+                City = "Indore",
+                State = "M.P.",
+                Zip = "452012",
+                PhoneNumber = "9845631405",
+                Email = "karanS145@gmail.com"
+            };
+            Contact con4 = new Contact
+            {
+                FirstName = "Nikita",
+                LastName = "Joshi",
+                Address = "Anand Nagar",
+                City = "Pune",
+                State = "M.H.",
+                Zip = "450004",
+                PhoneNumber = "7964850069",
+                Email = "Nikki123@gmail.com"
+            };
+            Contact con5 = new Contact
+            {
+                FirstName = "Akanksha",
+                LastName = "Patidar",
+                Address = "Gandhi Nagar",
+                City = "Ahemdabad",
+                State = "G.J.",
+                Zip = "456120",
+                PhoneNumber = "9764512999",
+                Email = "PatidarA444@gmail.com"
+            };
+           
+
+            book.AddContact(con1);
+            book.AddContact(con2);
+            book.AddContact(con3);
+            book.AddContact(con4);
+            book.AddContact(con5);
+
+            book.DisplayContacts();
+        }
+    }
+}
